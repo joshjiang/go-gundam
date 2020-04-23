e6730417c94b71c976038a971f6a65ddafd59fd9
Josh Jiang <josh.jiang.dbf@gmail.com>
2020-04-22 21:33:40 -0400

    Hello world, ebiten

--- code/main.go
+++ code/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil" // This is required to draw debug texts.
+)
 
-func main() {
-	fmt.Println("Hello World")
+func update(screen *ebiten.Image) error{
+	ebitenutil.DebugPrint(screen, "Our first game in Ebiten!")
+	return nil
+}
+
+func main(){
+	if err := ebiten.Run(update, 320, 240, 2, "Hello world!"); err!=nil{
+		panic(err)
+	}
 }
