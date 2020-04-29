5e84e1cbcf2e82ab92540777c3a536dcca92f0a2
Josh Jiang <josh.jiang.dbf@gmail.com>
2020-04-28 20:59:25 -0400

    first attempt at moving square

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Gundam 2D COVID-19 Fighter
+# Gundam AC Fighter
 
-This is a project to build a 2d game in golang. Here, you pilot a Gundam to build the Huoshenshan Hospital as fast as possible while COVID-19 spreads.
+This is a project to build a 2D game in golang.
 
--- code/main.go
+++ code/main.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil" // This is required to draw debug texts.
+	//"github.com/hajimehoshi/ebiten/ebitenutil" // This is required to draw debug texts.
+	"image/color"
 )
 
+// Goal #1: move square around on screen with keyboard
+var square *ebiten.Image
+
 func update(screen *ebiten.Image) error{
-	ebitenutil.DebugPrint(screen, "Our first game in Ebiten!")
+	if square == nil {
+		// create square
+		square, _ = ebiten.NewImage(16,16, ebiten.FilterNearest)
+	}
+
+	square.Fill(color.White)
+
+	opts := &ebiten.DrawImageOptions{}
+	var x float64 = 64
+	var y float64 = 64
+
+	opts.GeoM.Translate(x,y)
+
+
+	screen.DrawImage(square, opts)
+
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		opts.GeoM.Translate(x, y + 1)
+		screen.DrawImage(square, opts)
+	}
+
 	return nil
 }
+
+
 
 func main(){
 	if err := ebiten.Run(update, 320, 240, 2, "Hello world!"); err!=nil{
