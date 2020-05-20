ffe5266e478462069be097174f16e6f34837aff0
Josh Jiang <josh.jiang.dbf@gmail.com>
2020-05-19 23:48:06 -0400

    how to grab bomb?

--- code/main.go
+++ code/main.go
@@ -2,26 +2,43 @@ package main
 
 import (
 	"github.com/hajimehoshi/ebiten"
-	"log"
 	"github.com/hajimehoshi/ebiten/ebitenutil" // This is required to draw debug texts.
+	"log"
+	"math/rand"
 )
 
 // Goal #1: move square around on screen with keyboard
 const (
-	screenWidth, screenHeight = 640, 480
+	screenWidth, screenHeight = 1070, 1520
 )
 
 var (
 	err        error
 	background *ebiten.Image
 	gundam *ebiten.Image
+	napalm *ebiten.Image
 	playerOne player
+	bombOne bomb
 )
 
 type player struct {
 	image *ebiten.Image
 	xPos, yPos float64
 	speed	float64
+}
+
+type bomb struct {
+	image *ebiten.Image
+	xPos, yPos float64
+	isPickedUp bool
+}
+
+func abs(x float64) float64{
+	if x < 0{
+		val := x * -1
+		return val
+	}
+	return x
 }
 
 func movePlayer() {
@@ -38,23 +55,40 @@ func movePlayer() {
 		playerOne.xPos += playerOne.speed
 	}
 }
+
+func grabBomb(){
+	if abs(playerOne.yPos - bombOne.yPos) >= 10 && abs(playerOne.xPos - bombOne.xPos) >= 10{
+		bombOne.isPickedUp = true
+		for {
+			bombOne.xPos, bombOne.yPos = playerOne.xPos, playerOne.yPos + 20
+		}
+	}
+}
+
 // Run this code once at startup
 func init() {
-	background, _, err = ebitenutil.NewImageFromFile("../static/yugoslavia.png", ebiten.FilterDefault)
+	background, _, err = ebitenutil.NewImageFromFile("../static/korea.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	gundam, _, err = ebitenutil.NewImageFromFile("../static/Gundam.png", ebiten.FilterDefault)
+	gundam, _, err = ebitenutil.NewImageFromFile("../static/gundam.png", ebiten.FilterDefault)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	napalm, _, err = ebitenutil.NewImageFromFile("../static/napalm.png", ebiten.FilterDefault)
 	if err != nil{
 		log.Fatal(err)
 	}
 
 	playerOne = player{gundam, screenWidth/2.0, screenHeight/2.0, 4}
+	bombOne = bomb{napalm, rand.Float64()*screenWidth,rand.Float64()*screenHeight, false}
 }
 
 func update(screen *ebiten.Image) error{
 	movePlayer()
+	grabBomb()
 
 	if ebiten.IsDrawingSkipped() {
 		return nil
@@ -64,14 +98,20 @@ func update(screen *ebiten.Image) error{
 	screen.DrawImage(background,op)
 
 	playerOp := &ebiten.DrawImageOptions{}
-	playerOp.GeoM.Translate(playerOne.xPos,playerOne.yPos)
+	playerOp.GeoM.Scale(2.0,2.0)
+	playerOp.GeoM.Translate(playerOne.xPos, playerOne.yPos)
 	screen.DrawImage(playerOne.image, playerOp)
+
+	bombOp := &ebiten.DrawImageOptions{}
+	bombOp.GeoM.Scale(0.2,0.2)
+	bombOp.GeoM.Translate(bombOne.xPos, bombOne.yPos)
+	screen.DrawImage(bombOne.image, bombOp)
 
 	return nil
 }
 
 func main(){
-	if err := ebiten.Run(update, screenWidth, screenHeight, 1, "Hello world!"); err!=nil{
+	if err := ebiten.Run(update, screenWidth, screenHeight, .5, "Hello world!"); err!=nil{
 		panic(err)
 	}
 }
